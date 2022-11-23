fccd1cc85bfafe24495f26a46f49af5261435621
Elijah Green <elijah.khoury99@gmail.com>
2022-11-23 10:32:16 -0800

    commit tree 351a6a05fdad42a10385c02b821fa05f98cd338c

---
+++ index.js
@@ -0,0 +1,39 @@
+// //function add() {
+//     // we'll fill this in shortly
+//      return 'Error: Expected undefined to equal 1078'
+//     }
+//   function add(a, b) {
+//     return "1078"
+//}
+function add() {
+  return "something";
+}
+function add(a,b) {
+  return "something";
+}
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply (a,b) {
+  return a*b;
+}
+function divide (a,b) {
+  return a/b;
+}
+// // var n = 1078;
+
+function increment(n){
+  return n+=1
+}
+function decrement(n){
+  return n-=1
+}
+function makeInt(string){
+ return parseInt(string, 10)
+}
+function preserveDecimal(string){
+  return parseFloat(string,10) 
+}
